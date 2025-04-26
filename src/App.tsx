dd916564a3b31df1ab6aa2bc1cc297bc56408e4a
Rayched <ny2401@naver.com>
2025-04-26 21:07:52 +0900

    minutes -> hours 변환 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { HourSelectors, MinuteAtoms } from './atoms';
 
 function App() {
+  const Hours = useRecoilValue(HourSelectors);
+  const [Minutes, setMinutes] = useRecoilState(MinuteAtoms);
+
+  const MinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(event.currentTarget.value);
+  };
+
   return (
     <div>
       <h3>시간 단위 변환계</h3>
       <div>
         <label>시 Hour</label>
-        <input type='number' placeholder='Hours'/>
+        <input type='text' placeholder='Hours' value={Hours}/>
       </div>
       <div>
         <label>분 Minute</label>
-        <input type='number' placeholder='Minutes'/>
+        <input 
+          type='text' 
+          placeholder='Minutes'
+          value={Minutes}
+          onChange={MinutesChange}
+        />
       </div>
     </div>
   );
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,1 +1,15 @@
 import {atom, selector} from "recoil";
+
+export const MinuteAtoms = atom({
+    key: "MinuteAtoms",
+    default: ""
+});
+
+export const HourSelectors = selector({
+    key: "HourSelectors",
+    get: ({get}) => {
+        const MinuteValues = get(MinuteAtoms);
+        const Hours = MinuteValues !== "" ? Number(MinuteValues) / 60 : ""
+        return Hours;
+    },
+})
